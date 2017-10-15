73e5d661e30bef5c95e64355d35208091e5e6f6c
andrew <uandrew1965@gmail.com>
2017-10-15 17:07:19 +0300

    .

--- akka-extender/src/test/scala/com/simplesys/akka/http/AsyncContextMock.scala
+++ akka-extender/src/test/scala/com/simplesys/akka/http/AsyncContextMock.scala
@@ -1,32 +1,32 @@
-package com.simplesys.akka.http
-
-import java.io.{ByteArrayOutputStream, OutputStreamWriter, PrintWriter}
-import org.specs2.mock.Mockito
-import com.simplesys.servlet.{ServletResponse, ServletRequest, AsyncContext}
-import com.simplesys.servlet.http.{HttpServletResponse, HttpServletRequest}
-
-object AsyncContextMock extends Mockito {
-
-    def apply(): AsyncContext = {
-
-        val request = mock[HttpServletRequest]
-        val response = mock[HttpServletResponse]
-
-        request.Method returns "GET"
-        request.PathInfo returns "/test"
-
-        val asyncContext = mock[AsyncContext]
-
-        asyncContext.Timeout returns 1000
-
-        request.AsyncContext returns asyncContext
-
-        response.Writer returns new PrintWriter(new OutputStreamWriter(new ByteArrayOutputStream()))
-
-        request.StartAsync returns asyncContext
-        request.StartAsync(request, response) returns asyncContext
-
-        asyncContext.Request returns request
-        asyncContext.Response returns response
-    }
-}
+//package com.simplesys.akka.http
+//
+//import java.io.{ByteArrayOutputStream, OutputStreamWriter, PrintWriter}
+//import org.specs2.mock.Mockito
+//import com.simplesys.servlet.{ServletResponse, ServletRequest, AsyncContext}
+//import com.simplesys.servlet.http.{HttpServletResponse, HttpServletRequest}
+//
+//object AsyncContextMock extends Mockito {
+//
+//    def apply(): AsyncContext = {
+//
+//        val request = mock[HttpServletRequest]
+//        val response = mock[HttpServletResponse]
+//
+//        request.Method returns "GET"
+//        request.PathInfo returns "/test"
+//
+//        val asyncContext = mock[AsyncContext]
+//
+//        asyncContext.Timeout returns 1000
+//
+//        request.AsyncContext returns asyncContext
+//
+//        response.Writer returns new PrintWriter(new OutputStreamWriter(new ByteArrayOutputStream()))
+//
+//        request.StartAsync returns asyncContext
+//        request.StartAsync(request, response) returns asyncContext
+//
+//        asyncContext.Request returns request
+//        asyncContext.Response returns response
+//    }
+//}
