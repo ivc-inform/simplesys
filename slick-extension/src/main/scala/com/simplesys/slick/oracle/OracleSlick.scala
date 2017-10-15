3cd26c394c417877a06faa7240fbe2295eb51920
andrew <uandrew1965@gmail.com>
2017-10-15 21:03:35 +0300

    .

--- slick-extension/src/main/scala/com/simplesys/slick/oracle/OracleSlick.scala
+++ slick-extension/src/main/scala/com/simplesys/slick/oracle/OracleSlick.scala
@@ -1,7 +1,9 @@
 package com.simplesys.slick.oracle
 
-import com.simplesys.config.Config
+import com.typesafe.config.ConfigFactory
+import slick.jdbc.JdbcProfile
+import slick.basic.DatabaseConfig
 
 trait OracleSlick {
-
+    val profile: JdbcProfile = DatabaseConfig.forDriverClassName[JdbcProfile]("slick.jdbc.OracleProfile$", "slick", ConfigFactory.load()).profile
 }
