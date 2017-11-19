8f2112c817f84ed97a78ac630e4353578836900b
andrew <uandrew1965@gmail.com>
2017-11-19 11:31:28 +0300

    .

--- circe-extender/src/main/scala/com/simplesys/circe/Circe.scala
+++ circe-extender/src/main/scala/com/simplesys/circe/Circe.scala
@@ -2,7 +2,6 @@ package com.simplesys.circe
 
 import io.circe.Json
 
-
 object Circe {
     implicit class CirceOpt(json: Json) {
         def toPrettyprint = json.spaces4
--- servlet-wrapper/src/main/scala/com/simplesys/messages/Message.scala
+++ servlet-wrapper/src/main/scala/com/simplesys/messages/Message.scala
@@ -2,18 +2,18 @@ package com.simplesys.messages
 
 import com.simplesys.log.Logging
 import com.simplesys.servlet.http.sse.{SseChannel, SseEvent, SseID}
-import io.circe.{Json, JsonObject}
+import io.circe.Json
 import io.circe.Json._
 
 object Message {
     def apply(data: Json, channels: String*) = new Message(id = "", data = data, channels = channels.toSet, event = None)
-    def apply(channels: String*) = new Message(id = "", data = fromJsonObject(JsonObject.empty), channels = channels.toSet, event = None)
+    def apply(channels: String*) = new Message(id = "", data = arr(), channels = channels.toSet, event = None)
     def apply(id: String, data: Json) = new Message(id = id, data = data, channels = Set.empty[String], event = None)
 }
 
 object MessageExt {
     def apply(data: Json, channels: SseChannel*) = new Message(id = "", data = data, channels = channels.map(_.serrialize).toSet, event = None)
-    def apply(channels: SseChannel*) = new Message(id = "", data = fromJsonObject(JsonObject.empty), channels = channels.map(_.serrialize).toSet, event = None)
+    def apply(channels: SseChannel*) = new Message(id = "", data = arr(), channels = channels.map(_.serrialize).toSet, event = None)
     def apply(id: SseID, data: Json) = new Message(id = id.serrialize, data = data, channels = Set.empty[String], event = None)
 }
 
