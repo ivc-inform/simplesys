19436d831617e123efced103c49c9e88bd5060c7
andrew <uandrew1965@gmail.com>
2017-11-20 17:50:12 +0300

    .

--- common-webapp/src/main/scala/com/simplesys/isc/dataBinging/DSRequest.scala
+++ common-webapp/src/main/scala/com/simplesys/isc/dataBinging/DSRequest.scala
@@ -1,10 +1,7 @@
 package com.simplesys.isc.dataBinging
 
-import ch.qos.logback.core.db.dialect.SQLDialect
-import com.simplesys.sql.SQLDialect
-import io.circe.Decoder.Result
-import io.circe.{HCursor, Json}
 import com.simplesys.circe.Circe._
+import io.circe.Json
 
 case class DSRequest(data: Json, startRow: Option[Int] = None, endRow: Option[Int] = None) extends CirceHelper {
 
