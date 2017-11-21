819b08ca75811f78ab62760708cdcfb5a38becfb
andrew <uandrew1965@gmail.com>
2017-11-21 19:40:52 +0300

    .

--- common-webapp/src/main/scala/com/simplesys/isc/dataBinging/CirceHelper.scala
+++
@@ -1,6 +0,0 @@
-package com.simplesys.isc.dataBinging
-
-trait CirceHelper {
-    this: DSRequest ⇒
-    def toPrettyString = data.spaces4
-}
--- common-webapp/src/main/scala/com/simplesys/isc/dataBinging/DSRequest.scala
+++ common-webapp/src/main/scala/com/simplesys/isc/dataBinging/DSRequest.scala
@@ -8,7 +8,7 @@ import io.circe.Json._
 
 case class Transaction(transactionNum: Option[String] = None, operations: Seq[Json] = Seq.empty)
 
-case class DSRequest(data: Json, startRow: Option[Int] = None, endRow: Option[Int] = None, textMatchStyle: Option[String] = None, sortBy: Option[Json] = Some(fromString("exact")), transaction: Option[Transaction] = None, oldValues: Option[Json] = None) extends CirceHelper {
+case class DSRequest(data: Json, startRow: Option[Int] = None, endRow: Option[Int] = None, textMatchStyle: Option[String] = None, sortBy: Vector[Json] = Vector(fromString("exact")), transaction: Option[Transaction] = None, oldValues: Option[Json] = None) {
 
     def getString(key: String): String = data getString key
 
