eb8bcee5a16842d6cb0dc448d530add3ae4a392d
andrew <uandrew1965@gmail.com>
2017-11-21 14:37:31 +0300

    .

--- common-webapp/src/main/scala/com/simplesys/isc/dataBinging/DSResponse.scala
+++ common-webapp/src/main/scala/com/simplesys/isc/dataBinging/DSResponse.scala
@@ -1,11 +1,8 @@
 package com.simplesys.isc.dataBinging
 
-import com.simplesys.isc.system.global.RuntimeIscException
-import com.simplesys.jdbc.control.{ValidationEx, ValidationResult}
 import com.simplesys.log.Logging
-import io.circe.Json
-import io.circe.JsonObject
 import io.circe.Json._
+import io.circe.{Json, JsonObject}
 
 trait DSResponseBase {
     val data: Json
@@ -13,25 +10,13 @@ trait DSResponseBase {
     val totalRows: Option[Int]
 }
 case class DSResponse(data: Json, status: Int, totalRows: Option[Int] = None) extends DSResponseBase
-case class DSResponseFailureEx(result: ValidationEx[_]) extends DSResponseBase with Logging {
+
+case class DSResponseFailureEx(message: String, stackTrace: String) extends DSResponseBase with Logging {
 
 
     override val totalRows = None
 
     override val status = RPCResponse.statusFailure
-    
-    override val data: Json = if (result.printException.isEmpty)
-        Json.Null
-    else
-        fromJsonObject(JsonObject.singleton("error", fromJsonObject(JsonObject.fromIterable(Seq("message" → fromString(result.printException.get.message), "stackTrace" → fromString(result.printException.get.stackTrace))))))
 
-    result printException match {
-        case None =>
-            throw RuntimeIscException("Invalid branch")
-
-        case Some(ValidationResult(message, stackTrace)) =>
-            logger error s"message: $message"
-            logger error s"stackTrace: $stackTrace"
-
-    }
+    override val data = fromJsonObject(JsonObject.singleton("error", fromJsonObject(JsonObject.fromIterable(Seq("message" → fromString(message), "stackTrace" → fromString(stackTrace))))))
 }
