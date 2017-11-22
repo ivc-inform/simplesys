526093521d520ec860811267cb055c9cf9710b93
andrew <uandrew1965@gmail.com>
2017-11-22 14:59:20 +0300

    .

--- common-webapp/src/main/scala/com/simplesys/isc/dataBinging/DSRequest.scala
+++ common-webapp/src/main/scala/com/simplesys/isc/dataBinging/DSRequest.scala
@@ -3,42 +3,41 @@ package com.simplesys.isc.dataBinging
 import java.time.LocalDateTime
 
 import com.simplesys.circe.Circe._
-import io.circe.{Json, JsonObject}
-import io.circe.Json._
+import io.circe.Json
 
 case class Transaction(transactionNum: Option[String] = None, operations: Seq[Json] = Seq.empty)
 
-case class DSRequest(data: Json, startRow: Option[Int] = None, endRow: Option[Int] = None, textMatchStyle: Option[String] = None, sortBy: Vector[Json] = Vector.empty, transaction: Option[Transaction] = None, oldValues: Option[Json] = None) {
+case class DSRequest(request: Json, startRow: Option[Int] = None, endRow: Option[Int] = None, textMatchStyle: Option[String] = None, sortBy: Vector[Json] = Vector.empty, transaction: Option[Transaction] = None, oldValues: Option[Json] = None) {
 
-    def toPrettyString = data.toPrettyString
+    def toPrettyString = request.toPrettyString
 
-    def getString(key: String): String = data getString key
+    def getString(key: String): String = request getString key
 
-    def getStringOpt(key: String): Option[String] = data getStringOpt key
+    def getStringOpt(key: String): Option[String] = request getStringOpt key
 
-    def getLong(key: String): Long = data getLong key
+    def getLong(key: String): Long = request getLong key
 
-    def getLongOpt(key: String): Option[Long] = data getLongOpt key
+    def getLongOpt(key: String): Option[Long] = request getLongOpt key
 
-    def getDouble(key: String): Double = data getDouble key
+    def getDouble(key: String): Double = request getDouble key
 
-    def getDoubleOpt(key: String): Option[Double] = data getDoubleOpt key
+    def getDoubleOpt(key: String): Option[Double] = request getDoubleOpt key
 
-    def getLocalDateTime(key: String): LocalDateTime = data getLocalDateTime key
+    def getLocalDateTime(key: String): LocalDateTime = request getLocalDateTime key
 
-    def getLocalDateTimeOpt(key: String): Option[LocalDateTime] = data getLocalDateTimeOpt key
+    def getLocalDateTimeOpt(key: String): Option[LocalDateTime] = request getLocalDateTimeOpt key
 
-    def getJsonObject(key: String): Json = data getJsonObject key
+    def getJsonObject(key: String): Json = request getJsonObject key
 
-    def getJsonObjectOpt(key: String): Option[Json] = data getJsonObjectOpt key
+    def getJsonObjectOpt(key: String): Option[Json] = request getJsonObjectOpt key
 
-    def getBoolean(key: String): Boolean = data getBoolean key
+    def getBoolean(key: String): Boolean = request getBoolean key
 
-    def getBooleanOpt(key: String): Option[Boolean] = data getBooleanOpt key
+    def getBooleanOpt(key: String): Option[Boolean] = request getBooleanOpt key
 
-    def getJsonList(key: String): Vector[Json] = data getJsonList  key
+    def getJsonList(key: String): Vector[Json] = request getJsonList  key
 
-    def getJsonListOpt(key: String): Option[Vector[Json]] = data getJsonListOpt  key
+    def getJsonListOpt(key: String): Option[Vector[Json]] = request getJsonListOpt  key
 
-    def getJsonElement(key: String): Option[Json] = data getJsonElement key
+    def getJsonElement(key: String): Option[Json] = request getJsonElement key
 }
