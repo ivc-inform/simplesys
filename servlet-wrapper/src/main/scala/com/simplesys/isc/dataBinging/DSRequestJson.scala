4d43201b7bc2e155bc5e937b98ab79d60c8c7040
andrew <uandrew1965@gmail.com>
2017-11-24 11:04:20 +0300

    .

--- servlet-wrapper/src/main/scala/com/simplesys/isc/dataBinging/DSRequestJson.scala
+++ servlet-wrapper/src/main/scala/com/simplesys/isc/dataBinging/DSRequestJson.scala
@@ -4,4 +4,4 @@ import io.circe.Json
 
 case class Transaction(transactionNum: Option[String] = None, operations: Seq[Json] = Seq.empty)
 
-case class DSRequest(startRow: Option[Int] = None, endRow: Option[Int] = None, textMatchStyle: Option[String] = None, sortBy: Option[Vector[Json]] = None, transaction: Option[Transaction] = None, oldValues: Option[Json] = None)
+case class DSRequest(startRow: Option[Int] = None, endRow: Option[Int] = None, textMatchStyle: Option[String] = None, sortBy: Option[Vector[Json]] = None, transaction: Option[Transaction] = None, data: Option[Json] = None, oldValues: Option[Json] = None)
