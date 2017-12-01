a1ef4aa06a431666da56f849aa93a13981a29883
andrew <uandrew1965@gmail.com>
2017-12-01 14:10:49 +0300

    .

--- isc-misc/src/main/scala/com/simplesys/isc/system/misc/URL.scala
+++ isc-misc/src/main/scala/com/simplesys/isc/system/misc/URL.scala
@@ -19,11 +19,29 @@ class URL(variable: String, path: String) {
 
     override def toString = {
         def getPath = if (path.last === File.separatorChar) path.substring(0, path.length - 1) else path
-        if (variable !== strEmpty) variable.plus + getPath.dblQuoted else getPath.dblQuoted
+
+        if (variable !== strEmpty) {
+            variable.plus + {
+                import com.simplesys.common.JVM.Strings._
+                getPath.dblQuoted
+            }
+        } else {
+            import com.simplesys.common.JVM.Strings._
+            getPath.dblQuoted
+        }
     }
 
     def toStringWithLastSlash = {
         def getPath = if (path.last !== File.separatorChar) path.pathSlash else path
-        if (variable !== strEmpty) variable.plus + getPath.dblQuoted else getPath.dblQuoted
+
+        if (variable !== strEmpty) {
+            variable.plus + {
+                import com.simplesys.common.JVM.Strings._
+                getPath.dblQuoted
+            }
+        } else {
+            import com.simplesys.common.JVM.Strings._
+            getPath.dblQuoted
+        }
     }
 }
