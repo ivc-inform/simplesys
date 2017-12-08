bf772a67d3cd91791f4889cba91479c95ad0f06c
andrew <uandrew1965@gmail.com>
2017-12-08 11:47:16 +0300

    .

--- slick-extension/src/main/scala/com/simplesys/slick/SlickDriver.scala
+++ slick-extension/src/main/scala/com/simplesys/slick/SlickDriver.scala
@@ -9,11 +9,11 @@ import slick.jdbc.JdbcBackend.Database
 import slick.jdbc.JdbcProfile
 
 object SlickDriver {
-    def forDriverName(basePath:String, config: Config): JdbcProfile = DatabaseConfig.forConfig[JdbcProfile](basePath, config).profile
+    def forDriverName(basePath: String, config: Config): JdbcProfile = DatabaseConfig.forConfig[JdbcProfile](basePath, config).profile
 }
 
 object SlickDatabase {
-    def forConfig(basePath:String,  config: Config, slickConfiguration: SlickConfiguration): Database = {
+    def forConfig(basePath: String, config: Config, slickConfiguration: SlickConfiguration): Database = {
         val jndiName = slickConfiguration.jndiName.map(Database.forName(_, None))
         val jndiDbName = slickConfiguration.jndiDbName.map(new InitialContext().lookup(_).asInstanceOf[Database])
         jndiName.orElse(jndiDbName).getOrElse(Database.forConfig(basePath, config))
