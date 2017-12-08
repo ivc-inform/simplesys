7bf5f92917d1c11387b9ec218b77db6b6f80c108
andrew <uandrew1965@gmail.com>
2017-12-08 13:05:37 +0300

    .

--- slick-extension/src/main/scala/com/simplesys/slick/oracle/OracleSlick.scala
+++ slick-extension/src/main/scala/com/simplesys/slick/oracle/OracleSlick.scala
@@ -6,6 +6,7 @@ import slick.basic.DatabaseConfig
 
 trait OracleSlick {
     val databaseConfig: DatabaseConfig[JdbcProfile] = DatabaseConfig.forDriverClassName[JdbcProfile]("slick.jdbc.OracleProfile$", "slick", ConfigFactory.load())
+    //val databaseConfig: DatabaseConfig[JdbcProfile] = DatabaseConfig.forConfig[JdbcProfile]("slick", ConfigFactory.load())
     val db = databaseConfig.db
     val profile = databaseConfig.profile
 }
