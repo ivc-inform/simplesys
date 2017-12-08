2ae0df263f1b3a889f129dec9e2505949bc602e4
andrew <uandrew1965@gmail.com>
2017-12-08 14:50:13 +0300

    .

--- slick-extension/src/test/scala/test/TestSuit.scala
+++ slick-extension/src/test/scala/test/TestSuit.scala
@@ -9,9 +9,10 @@ import scala.concurrent.ExecutionContext.Implicits.global
 
 class TestSuit extends FunSuite with OracleSlick {
     test("1") {
-        val dbb = OracleSlick.getDataBaseConfig(url = "jdbc:oracle:thin:@//dev.db-support.ru:1521/orcl", user = "temp", password = "temp")
-        //val dbb = slick.jdbc.OracleProfile.api.Database.forURL(url = "jdbc:oracle:thin:@//dev.db-support.ru:1521/orcl", user = "temp", password = "temp", driver = "slick.jdbc.OracleProfile$")
-        val res: DBIOAction[Seq[MQName], NoStream, Effect.All] = dbb.profile.defaultTables.map(_ map (_.name))
-        val a = res
+        //val dbb = OracleSlick.getDataBaseConfig(url = "jdbc:oracle:thin:@//dev.db-support.ru:1521/orcl", user = "temp", password = "temp").db
+        val dbb = slick.jdbc.OracleProfile.api.Database.forURL(url = "jdbc:oracle:thin:@//dev.db-support.ru:1521/orcl", user = "temp", password = "temp", driver = "oracle.jdbc.OracleDriver")
+        dbb.source.createConnection()
+//        val res: DBIOAction[Seq[MQName], NoStream, Effect.All] = dbb.profile.defaultTables.map(_ map (_.name))
+//        val a = res
     }
 }
