e83a21a841b585a3e189b8868b775282e5362354
andrew <uandrew1965@gmail.com>
2017-12-08 17:37:09 +0300

    .

--- slick-extension/src/main/scala/com/simplesys/slick/oracle/time/package.scala
+++ slick-extension/src/main/scala/com/simplesys/slick/oracle/time/package.scala
@@ -1,14 +1,26 @@
 package com.simplesys.slick.oracle
 
 import java.sql.Timestamp
-import java.time.LocalDateTime
+import java.time.{LocalDate, LocalDateTime, ZonedDateTime}
 
 package object time {
     val profile = slick.jdbc.OracleProfile
+
     import profile.api._
 
     implicit val localDateToDate = MappedColumnType.base[LocalDateTime, Timestamp](
         l => Timestamp.valueOf(l),
         d => d.toLocalDateTime
+    )
+
+    implicit val javaLocalDateMeta = MappedColumnType.base[LocalDate, Timestamp](
+        ldt => Timestamp.valueOf(ldt.atStartOfDay()),
+        t => t.toLocalDateTime.toLocalDate,
+    )
+
+
+    implicit val javaDateTimeMeta = MappedColumnType.base[ZonedDateTime, Timestamp](
+        (dt) => java.sql.Timestamp.valueOf(dt.toLocalDateTime),
+        (t) => t.toLocalDateTime.atZone(java.time.ZoneOffset.UTC)
     )
 }
