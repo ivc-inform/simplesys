781fb9cc4203561c60c251f2ead5472afe0b1002
andrew <uandrew1965@gmail.com>
2017-12-08 14:00:03 +0300

    .

--- slick-extension/src/main/scala/com/simplesys/slick/oracle/OracleSlick.scala
+++ slick-extension/src/main/scala/com/simplesys/slick/oracle/OracleSlick.scala
@@ -1,12 +1,33 @@
 package com.simplesys.slick.oracle
 
 import com.typesafe.config.ConfigFactory
-import slick.jdbc.JdbcProfile
+import slick.jdbc.{JdbcBackend, JdbcProfile}
 import slick.basic.DatabaseConfig
+import com.simplesys.common.Strings._
 
 trait OracleSlick {
-    val databaseConfig: DatabaseConfig[JdbcProfile] = DatabaseConfig.forDriverClassName[JdbcProfile]("slick.jdbc.OracleProfile$", "slick", ConfigFactory.load())
     //val databaseConfig: DatabaseConfig[JdbcProfile] = DatabaseConfig.forConfig[JdbcProfile]("slick", ConfigFactory.load())
+    val databaseConfig: DatabaseConfig[JdbcProfile] = DatabaseConfig.forConfig[JdbcProfile]("slick", ConfigFactory.parseString(
+        """slick {
+          |    db {
+          |      url = "jdbc:oracle:thin:@//dev.db-support.ru:1521/orcl"
+          |      user = "temp"
+          |      password = "temp"
+          |  }
+          |     driver = "slick.jdbc.OracleProfile$"
+          |}""".stripMargin))
     val db = databaseConfig.db
     val profile = databaseConfig.profile
+}
+
+object OracleSlick {
+    def getDataBaseConfig(url: String, user: String, password: String, driver: String = "slick.jdbc.OracleProfile$") = DatabaseConfig.forConfig[JdbcProfile]("slick", ConfigFactory.parseString(
+        """slick {
+            db {
+              url = %s
+              user = %s
+              password = %s
+          }
+             driver = %s
+        }""".stripMargin.format(url.dblQuoted, user.dblQuoted, password.dblQuoted, driver.dblQuoted)))
 }
--- slick-extension/src/test/scala/test/TestSuit.scala
+++ slick-extension/src/test/scala/test/TestSuit.scala
@@ -3,8 +3,9 @@ package test
 import com.simplesys.slick.oracle.OracleSlick
 import org.scalatest.FunSuite
 
-class TestSuit extends FunSuite with OracleSlick{
-  test("1") {
-      val dbb = db
-  }
+class TestSuit extends FunSuite with OracleSlick {
+    test("1") {
+        val dbb = OracleSlick.getDataBaseConfig(url = "jdbc:oracle:thin:@//dev.db-support.ru:1521/orcl", user = "temp", password = "temp").db
+        dbb.createSession()
+    }
 }
