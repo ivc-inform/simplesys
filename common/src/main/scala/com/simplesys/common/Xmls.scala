641f0ddfb7151c648a802b6a8ffebe422d2fcecb
andrew <uandrew1965@gmail.com>
2017-12-13 15:30:18 +0300

    .

--- common/src/main/scala/com/simplesys/common/Xmls.scala
+++ common/src/main/scala/com/simplesys/common/Xmls.scala
@@ -1,15 +1,25 @@
 package com.simplesys.common
 
-import scala.xml.NodeSeq
+import scala.xml.{Elem, Node, NodeSeq, PrettyPrinter}
 
 
 object XMLs {
-  implicit class NodeSeqWrapper(nodeSeq: NodeSeq) {
-    def textOption: Option[String] = {
-      val text = nodeSeq.text
-      if (text == null || text.isEmpty) None else Some(text)
+    implicit class NodeSeqWrapper(nodeSeq: NodeSeq) {
+        def textOption: Option[String] = {
+            val text = nodeSeq.text
+            if (text == null || text.isEmpty) None else Some(text)
+        }
     }
-  }
+
+    def p(width: Int = 250, step: Int = 4) = new PrettyPrinter(width, step)
+
+    implicit class ElemOpt(elem: Elem) {
+        def toPrettyString(width: Int = 250, step: Int = 4): String = p(width, step).format(elem)
+    }
+
+    implicit class NodeOpt(elem: Node) {
+        def toPrettyString(width: Int = 250, step: Int = 4): String = p(width, step).format(elem)
+    }
 }
 
 
